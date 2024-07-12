4551bb5c8bc88eb5658321226ed61758e1e1e4ae
Kavi-Arya <110332389+Kavi-Arya@users.noreply.github.com>
2024-07-12 18:38:22 +0530

    Add files via upload

---
+++ dmenu/colors/everforest.h
@@ -0,0 +1,9 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d8caac", "#323d43" },
+	[SchemeSel] = { "#000000", "#a7c080" },
+	[SchemeSelHighlight] = { "#d98f80", "#000000" },
+	[SchemeNormHighlight] = { "#d98f80", "#000000" },
+	[SchemeOut] = { "#ffffff", "#83b6af" },
+	[SchemeMid] = { "#d8caac", "#323d43" },
+};
---
+++ dmenu/colors/gruvbox-dark.h
@@ -0,0 +1,9 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d4be98", "#1d2021" },
+	[SchemeSel] = { "#000000", "#ea6962" },
+	[SchemeSelHighlight] = { "#d8a657", "#000000" },
+	[SchemeNormHighlight] = { "#d8a657", "#000000" },
+	[SchemeOut] = { "#ffffff", "#89b482" },
+	[SchemeMid] = { "#d4be98", "#282828" },
+};
